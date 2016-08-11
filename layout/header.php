05c6a579030c0545c98b1c982b2c25ca0ca1ffbd
Orobo Lucky Ozoka <orobogenius@gmail.com>
2016-08-11 09:13:34 +0100

    Added authentication
    Added authentication to the header which will display different header content depending on whether or not the user is signed in.

--- layout/header.php
+++ layout/header.php
@@ -1,38 +1,55 @@
 <?php
 
-$header = <<<END
-  <body>
-    <div class="forum-header">
-      <div class="header-left">
-        <ul>
-          <li>
-            <a href="http://localhost/codeclassforum/views/login.php">Login</a>
-          </li>
-          <li>
-            <a href="http://localhost/codeclassforum/views/registration.php">Register</a>
-          </li>
-        </ul>
-      </div>
-      <div class="header-right">
-        <form action="http://localhost/codeclassforum/views/login.php" method="post">
-          <ul>
-            <li>
-              <label for="username">Username: &nbsp;</label>
-              <input type="text" name="username" id="username" required />
-            </li>
-            <li>
-              <label for="password">Password: &nbsp;</label>
-              <input type="password" name="password" id="password" required />
-            </li>
-            <li>
-              <input type="submit" value="Login" id="login-btn" />
-            </li>
-          </ul>
-        </form>
-      </div>
-    </div>
-END;
+session_start();
 
-echo $header;
+echo '<body>';
+  echo '<div class="forum-header">';
+  if (!isset($_SESSION['username'])) {
+    //User is not logged in
+    echo '<div class="header-left">';
+    echo '<ul>';
+      echo '<li>';
+        echo '<a href="http://localhost/codeclassforum/views/login.php">Login</a>';
+      echo '</li>';
+      echo '<li>';
+          echo '<a href="http://localhost/codeclassforum/views/registration.php">Register</a>';
+      echo '</li>';
+    echo '</ul>';
+    echo '</div>';
+  }
+    echo '<div class="header-right">';
+  if (isset($_SESSION['username'])) {
+    //User is loggged in
+    echo '<ul>';
+      echo '<li>';
+        echo '<img src="http://localhost/codeclassforum/images/profile.png" /> &nbsp; | ';
+      echo '</li>';
+      echo '<li>';
+        echo 'Welcome, '.$_SESSION['username'] . ' | ';
+      echo '</li>';
+      echo '<li>';
+        echo '<a href="http://localhost/codeclassforum/logout.php">Sign Out</a>';
+      echo '</li>';
+    echo '</ul>';
+  } else {
+    //User is not logged in, show login form
+    echo '<form action="http://localhost/codeclassforum/views/login.php" method="post">';
+      echo '<ul>';
+        echo '<li>';
+          echo '<label for="username">Username: &nbsp;</label>';
+          echo '<input type="text" name="username" id="username" required />';
+        echo '</li>';
+        echo '<li>';
+          echo '<label for="password">Password: &nbsp;</label>';
+          echo '<input type="password" name="password" id="password" required />';
+        echo '</li>';
+        echo '<li>';
+          echo '<input type="submit" value="Login" id="login-btn" />';
+        echo '</li>';
+      echo '</ul>';
+    echo '</form>';
+  }
+  echo '</div>';
+echo '</div>';
 
 ?>
