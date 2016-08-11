8aef31aaf0f04e868c105041939f42cab104ba7e
Orobo Lucky Ozoka <orobogenius@gmail.com>
2016-08-11 09:19:42 +0100

    Added new functions
    The following changes were made:
    
    1. Changed the addThread function signature - added the userID parameter to include the id of the user creating the thread.
    2. Added the getTopics function - this gets all the topics/threads belonging to the specified cat_id (category).
    3. Added the getUserID function - this gets the userID of a user specified by the username parameter.
    4. Added the getUserLevel function - this gets the user level of a user specified by the username parameter.
    5. Added the getUsers function - this gets all users in the forum

--- utility/db_functions.php
+++ utility/db_functions.php
@@ -22,7 +22,8 @@
   }
 
   function registerUser($dbh, $firstname, $lastname, $gender, $username, $password, $email, $age) {
-    $query = "INSERT INTO users VALUES ('', '$firstname', '$lastname', '$gender', '$age', '$username', '$password', '$email', '1', 'now()')";
+    $date = Date('Y-m-d H:i:s');
+    $query = "INSERT INTO users VALUES ('', '$firstname', '$lastname', '$gender', '$age', '$username', '$password', '$email', '1', '$date')";
     $result = mysqli_query($dbh, $query) or die(mysqli_connect_errno($dbh));
     if ($result) {
       //User registration is successful
@@ -78,11 +79,41 @@
     return $response;
   }
 
-  function addThread($dbh, $name, $description, $cat_id) {
+  function getUsers($dbh) {
+    $response = array();
+    $query = "SELECT * FROM users";
+    $result = mysqli_query($dbh, $query) or die(mysqli_connect_errno($dbh));
+    while($row = mysqli_fetch_array($result)) {
+      $response[] = $row;
+    }
+    return $response;
+  }
+
+  function getUserLevel($dbh, $username) {
+    $userlevel = 0;
+    $query = "SELECT user_level FROM users WHERE user_name = '$username'";
+    $result = mysqli_query($dbh, $query) or die(mysqli_connect_errno($dbh));
+    while ($row = mysqli_fetch_array($result)) {
+        $userlevel = $row['user_level'];
+    }
+    return $userlevel;
+  }
+
+  function getUserID($dbh, $username) {
+    $userID = 0;
+    $query = "SELECT user_id FROM users WHERE user_name = '$username'";
+    $result = mysqli_query($dbh, $query) or die(mysqli_connect_errno($dbh));
+    while ($row = mysqli_fetch_array($result)) {
+        $userID = $row['user_id'];
+    }
+    return $userID;
+  }
+
+  function addThread($dbh, $name, $description, $cat_id, $userID) {
     //Y-m-d H:i:s
     //2016-08-08 17:05:10
     $date = Date('Y-m-d H:i:s');
-    $query = "INSERT INTO threads VALUES ('', '$name', '$date', '$description', '$cat_id', '4')";
+    $query = "INSERT INTO threads VALUES ('', '$name', '$date', '$description', '$cat_id', '$userID')";
     $result = mysqli_query($dbh, $query) or die(mysqli_connect_errno($dbh));
     if ($result) {
       return true;
