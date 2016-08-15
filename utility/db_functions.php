ca63cf1a42315b99990c7f4cdc09df7d5fe2da3b
Orobo Lucky Ozoka <orobogenius@gmail.com>
2016-08-15 12:45:21 +0100

    Update db_functions.php

--- utility/db_functions.php
+++ utility/db_functions.php
@@ -109,13 +109,34 @@
     return $userID;
   }
 
-  function addThread($dbh, $name, $description, $cat_id, $userID) {
+  function getUserData($dbh, $user_id) {
+    $response = array();
+    $query = "SELECT * FROM users WHERE user_id = '$user_id'";
+    $result = mysqli_query($dbh, $query) or die(mysqli_connect_errno($dbh));
+    while ($row = mysqli_fetch_array($result)){
+      $response[] = $row;
+    }
+    return $response;
+  }
+
+  function getNumberOfPosts($dbh, $userID) {
+    $response = array();
+    $query = "SELECT * FROM posts WHERE post_by = '$userID'";
+    $result = mysqli_query($dbh, $query) or die(mysqli_connect_errno($dbh));
+    while ($row = mysqli_fetch_array($result)){
+      $response[] = $row;
+    }
+    return $response;
+  }
+
+  function addThread($dbh, $name, $description, $post, $cat_id, $userID) {
     //Y-m-d H:i:s
     //2016-08-08 17:05:10
     $date = Date('Y-m-d H:i:s');
-    $query = "INSERT INTO threads VALUES ('', '$name', '$date', '$description', '$cat_id', '$userID')";
+    $query = "INSERT INTO threads VALUES ('', '$name', '$date', '$description', '$post', '$cat_id', '$userID')";
     $result = mysqli_query($dbh, $query) or die(mysqli_connect_errno($dbh));
     if ($result) {
+      addPostToThread($dbh, $name, $post, $cat_id, mysqli_insert_id($dbh), getUserID($dbh, $_SESSION['username']));
       return true;
     } else return false;
   }
@@ -123,6 +144,67 @@
   function getTopics($dbh, $cat_id) {
     $response = array();
     $query = "SELECT * FROM threads WHERE cat_id = '$cat_id'";
+    $result = mysqli_query($dbh, $query) or die(mysqli_connect_errno($dbh));
+    while ($row = mysqli_fetch_array($result)) {
+      $response[] = $row;
+    }
+    return $response;
+  }
+
+  function getThreadID($dbh, $name, $cat_id) {
+    $thread_id = 0;
+    $query = "SELECT thread_id FROM threads WHERE thread_name = '$name' AND cat_id = 'cat_id'";
+    $result = mysqli_query($dbh, $query) or die(mysqli_connect_errno($dbh));
+    while ($row = mysqli_fetch_array($result)) {
+      $thread_id = $row['thread_id'];
+      echo $row['thread_id'];
+    }
+    return $thread_id;
+  }
+
+  function addPostToThread($dbh, $name, $post, $cat_id, $thread_id, $userID) {
+    $date = Date('Y-m-d H:i:s');
+    $query = "INSERT INTO posts VALUES ('', '$post', '$date', '$thread_id', '$userID')";
+    $result = mysqli_query($dbh, $query) or die(mysqli_connect_errno($dbh));
+    if ($result) {
+      return true;
+    } else return false;
+  }
+
+  function postReply($dbh, $reply, $userID, $thread_id) {
+    $date = Date('Y-m-d H:i:s');
+    $query = "INSERT INTO posts VALUES ('', '$reply', '$date', '$thread_id', '$userID')";
+    $result = mysqli_query($dbh, $query) or die(mysqli_connect_errno($dbh));
+    if ($result) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function getThreadData($dbh, $thread_id) {
+    $response = array();
+    $query = "SELECT * FROM threads WHERE thread_id = '$thread_id'";
+    $result = mysqli_query($dbh, $query) or die(mysqli_connect_errno($dbh));
+    while ($row = mysqli_fetch_array($result)) {
+      $response[] = $row;
+    }
+    return $response;
+  }
+
+  function getPosts($dbh, $thread_id) {
+    $response = array();
+    $query = "SELECT * FROM posts WHERE post_thread = '$thread_id'";
+    $result = mysqli_query($dbh, $query) or die(mysqli_connect_errno($dbh));
+    while ($row = mysqli_fetch_array($result)) {
+      $response[] = $row;
+    }
+    return $response;
+  }
+
+  function getLastPost($dbh, $thread_id) {
+    $response = array();
+    $query = "SELECT * FROM posts WHERE post_thread = '$thread_id' ORDER BY post_date desc LIMIT 1";
     $result = mysqli_query($dbh, $query) or die(mysqli_connect_errno($dbh));
     while ($row = mysqli_fetch_array($result)) {
       $response[] = $row;
